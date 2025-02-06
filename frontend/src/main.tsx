f6bedac2cbdc9b4bf599c9e344bc6c82b26227e3
Carlos Galindo <utm22030587@utma.edu.mx>
2025-02-06 16:07:13 -0600

    feat: set up auth0 provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { Auth0Provider } from '@auth0/auth0-react';
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Auth0Provider
+    domain='dev-onyfbauj3gubi6i8.us.auth0.com'
+    clientId='6ru6RzdkPMDyhETRAoC837xuwxPkpqwI'
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </StrictMode>,
+  </Auth0Provider>,
 )
