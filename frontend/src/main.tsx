abb0a68034e80b43e772fa00032d3e6fa02c7c54
Carlos Galindo <utm22030587@utma.edu.mx>
2025-02-06 21:38:22 -0600

    feat: add environment variables

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -7,10 +6,11 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 createRoot(document.getElementById('root')!).render(
   <Auth0Provider
-    domain='dev-onyfbauj3gubi6i8.us.auth0.com'
-    clientId='6ru6RzdkPMDyhETRAoC837xuwxPkpqwI'
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
+      audience: `${import.meta.env.VITE_AUTH0_DOMAIN}/api/v2/`
     }}
   >
     <App />
